03231eb896c575efcf9f61db3880994271b2704e
usermrlin <lin950821@163.com>
2023-11-23 16:53:29 +0800

    引入vant组件库，引入sass修改主题色

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
   </div>
 </template>
+
+<style lang="scss">
+:root {
+  --van-primary-color: #fb7299 !important;
+  --van-back-top-background: #fbfbfb !important;
+  --van-back-top-text-color: #666 !important;
+}
+</style>
